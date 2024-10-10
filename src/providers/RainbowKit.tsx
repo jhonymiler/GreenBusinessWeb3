2f25d7a520718d3487fb08350296fcc6e77f5d61
Jhony Miler <jonatasmiler@gmail.com>
2024-10-10 20:33:31 -0300

    Iniciando estruturas

---
+++ src/providers/RainbowKit.tsx
@@ -0,0 +1,22 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    getDefaultConfig,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+} from 'wagmi/chains';
+import {
+    QueryClientProvider,
+    QueryClient,
+} from "@tanstack/react-query";
+
+export default function RainbowKit() {
+    return (<div>Teste</div>)
+
+}
