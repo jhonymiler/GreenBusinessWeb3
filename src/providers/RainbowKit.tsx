17b66a3ae1e0fdc3265105e64b1bf8e9d7d04c5b
Jhony Miler <jonatasmiler@gmail.com>
2024-10-10 20:55:37 -0300

    Adicionando providers para conexão wallet

--- src/providers/RainbowKit.tsx
+++ src/providers/RainbowKit.tsx
@@ -1,22 +1,28 @@
+import React from 'react';
 import '@rainbow-me/rainbowkit/styles.css';
-import {
-    getDefaultConfig,
-    RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
-import {
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-} from 'wagmi/chains';
-import {
-    QueryClientProvider,
-    QueryClient,
-} from "@tanstack/react-query";
+import { polygon } from 'wagmi/chains';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-export default function RainbowKit() {
-    return (<div>Teste</div>)
+const config = getDefaultConfig({
+    appName: 'Empresa Verde',
+    chains: [polygon],
+    projectId: 'empresa-verde',
+});
 
+type RanbowKitProps = {
+    children: React.ReactNode;
+};
+
+export default function RainbowKit({ children }: RanbowKitProps) {
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={new QueryClient()}>
+                <RainbowKitProvider >
+                    {children}
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    )
 }
