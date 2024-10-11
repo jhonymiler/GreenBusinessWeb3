9e07fc110be25dec795e290e09825f0a01d0984b
Jhony Miler <jonatasmiler@gmail.com>
2024-10-10 21:52:48 -0300

    Linkando com Ganache

--- src/providers/RainbowKitUIProvider.tsx
+++ src/providers/RainbowKitUIProvider.tsx
@@ -5,10 +5,24 @@ import { WagmiProvider } from 'wagmi';
 import { polygon, mainnet } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
+const localChain = {
+    id: Number(process.env.NEXT_PUBLIC_CHAIN_ID), // Conversão explícita para número
+    name: 'Ganache',
+    nativeCurrency: {
+        decimals: 18,
+        name: 'Ether',
+        symbol: 'ETH',
+    },
+    rpcUrls: {
+        default: { http: [process.env.NEXT_PUBLIC_RPC_URL as string] },
+    },
+};
+
+
 const config = getDefaultConfig({
     appName: 'Empresa Verde',
-    chains: [polygon, mainnet],
-    projectId: '35f812c4964ad150854e578fcb2792fb',
+    chains: [localChain, polygon, mainnet,],
+    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_KEY as string,
 });
 
 type RanbowKitProps = {
